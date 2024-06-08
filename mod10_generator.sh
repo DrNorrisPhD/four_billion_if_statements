9a4ea407547e14df332b97086e5f0f001b7753d4
DrNorrisPhD <68154973+DrNorrisPhD@users.noreply.github.com>
2024-06-07 21:19:42 -0400

    Update mod10_generator.sh

--- mod10_generator.sh
+++ mod10_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf '#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char* argv[]) {\n' > ./mod10.c
-printf 'int x = atoi(argv[1]);' >> ./mod10.c
+printf '\tint x = atoi(argv[1]);\n' >> ./mod10.c
 printf '\tif (x == 0) return 0;\n' >> ./mod10.c
 for ((i=1;i<=4294967295;i++))
 do
