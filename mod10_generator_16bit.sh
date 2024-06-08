4291b2f5b63689a469f40b00248c16781b941395
DrNorrisPhD <68154973+DrNorrisPhD@users.noreply.github.com>
2024-06-07 21:19:57 -0400

    Update mod10_generator_16bit.sh

--- mod10_generator_16bit.sh
+++ mod10_generator_16bit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf '#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char* argv[]) {\n' > ./mod10_16bit.c
-printf 'int x = atoi(argv[1]);' >> ./mod10_16bit.c
+printf '\tint x = atoi(argv[1]);\n' >> ./mod10_16bit.c
 printf '\tif (x == 0) return 0;\n' >> ./mod10_16bit.c
 for ((i=1;i<=65535;i++))
 do
