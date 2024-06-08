07707b90610997989adc15e2495f7f5f85d2525f
DrNorrisPhD <68154973+DrNorrisPhD@users.noreply.github.com>
2024-06-07 23:13:42 -0400

    Update mod10_generator_16bit.sh

--- mod10_generator_16bit.sh
+++ mod10_generator_16bit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf '#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char* argv[]) {\n' > ./mod10_16bit.c
+printf '#include <stdlib.h>\n\nint main(int argc, char* argv[]) {\n' > ./mod10_16bit.c
 printf '\tint x = atoi(argv[1]);\n' >> ./mod10_16bit.c
 printf '\tif (x == 0) return 0;\n' >> ./mod10_16bit.c
 for ((i=1;i<=65535;i++))
